4016b8b9f61c717b1934a192c0d94505240f4051
Daniel Rothmaler <drothmaler@users.noreply.github.com>
2019-12-19 17:56:49 +0100

    Include local 'man' directory

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
 export PATH="/usr/libexec:./bin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$ZSH/man:$MANPATH"
